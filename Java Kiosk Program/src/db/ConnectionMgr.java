9873e6afcab4cd366204f01f21686c55c789f95b
haeun Noh <okhagrace06@naver.com>
2022-11-19 10:00:16 +0900

    Add files via upload

---
+++ Java Kiosk Program/src/db/ConnectionMgr.java
@@ -0,0 +1,49 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionMgr {
+	//프로그램에서 한 개만 사용할 목적으로 static변수로 선언
+	private static Connection con;
+	//static으로 
+	static {
+		try {
+			//MySQL 드라이버 로딩
+			Class.forName(DB.MySQL.DRIVER_NAME);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//Connection 가져오기
+	public static Connection getConnection() {
+		//없으면 만들고 리턴
+		//있으면 있는거 리턴
+		if(con == null) {
+			con = makeConnection();
+		}
+		return con;
+	}
+	//Connection 끊기
+	public static void closeConnection() {
+		if(con != null) {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			con = null;
+		}
+	}
+	//Connection 생성
+	private static Connection makeConnection() {
+		try {
+			con = DriverManager.getConnection(DB.MySQL.JDBC_URL);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+}
---
+++ Java Kiosk Program/src/db/MenuDAO.java
@@ -0,0 +1,79 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import dto.MenuDTO;
+
+public class MenuDAO {
+	Connection con;
+	PreparedStatement pstmt;
+	ResultSet rs;
+	
+	public MenuDAO() {
+		this.con = ConnectionMgr.getConnection();
+	}
+	
+	//메뉴 추가하는 작업
+	
+	
+	//메뉴 수정하는 작업
+	
+	//메뉴 삭제하는 작업
+	
+	//메뉴 카테고리별로 조회하는 작업
+	public List<MenuDTO> getMenuListByCategory(String category){
+		//MenuDTO를 담을 공간 준비
+		List<MenuDTO> menuList =  new ArrayList<>();
+		
+		String sql = "select * from menu where menu_category = ?";
+		try {
+			this.pstmt = con.prepareStatement(sql);
+			this.pstmt.setString(1, category);
+			this.rs = this.pstmt.executeQuery();
+			while(rs.next()) {
+				//각 칼럼명으로 읽기
+				int menuId = rs.getInt("menu_id");
+				String menuName = rs.getString("menu_name");
+				String menuImageFileName = rs.getString("menu_img_path");
+				int menuPrice = rs.getInt("menu_price");
+				String menuCategory = rs.getString("menu_category");
+				
+				//데이터 읽어서 menuList에 추가
+				MenuDTO menu = new MenuDTO(menuId, menuName, menuPrice, menuImageFileName);
+				menuList.add(menu);
+				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if(this.pstmt != null) {
+				try {
+					this.pstmt.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				this.pstmt = null;
+			}
+			if(this.rs != null) {
+				try {
+					this.rs.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				this.rs = null;
+			}
+		}
+		
+		return menuList;
+	}
+	
+	//메뉴 전체 리스트 조회하는 작업
+}
