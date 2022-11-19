e6aef0b461e9f3f626968e21a2db2020e203d0a8
haeun Noh <okhagrace06@naver.com>
2022-11-19 10:07:19 +0900

    Delete DB.java

--- Java Kiosk Program/db/DB.java
+++
@@ -1,8 +0,0 @@
-package db;
-
-public class DB {
-	public static class MySQL{
-		public static String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
-		public static String JDBC_URL = "jdbc:mysql://localhost/kioskdb?user=1109&password=Gkdms~!1357";
-	}
-}
