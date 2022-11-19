10ff3f6d28ac1cc0d6a518cd6083a84b20c8a824
haeun Noh <okhagrace06@naver.com>
2022-11-19 09:56:02 +0900

    Add files via upload

---
+++ Java Kiosk Program/src/Frame/Main.java
@@ -0,0 +1,11 @@
+package Frame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new KioskStartFrame();
+//		new OptionPopUp();
+//		new OrderFrame();
+//		new MenuFrame();
+	}
+}
