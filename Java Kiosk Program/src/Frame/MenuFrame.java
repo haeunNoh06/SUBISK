bf48a5c05f102612b025391f185f86d58cbaba4d
haeun Noh <okhagrace06@naver.com>
2022-11-23 13:45:15 +0900

    Update MenuFrame.java

--- Java Kiosk Program/src/Frame/MenuFrame.java
+++ Java Kiosk Program/src/Frame/MenuFrame.java
@@ -2,41 +2,32 @@ package Frame;
 
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.ScrollPane;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ComponentListener;
-import java.awt.event.KeyEvent;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
-import javax.swing.text.AbstractDocument.Content;
 
-import db.DB;
 import db.MenuDAO;
-import db.DB.*;
 import dto.MenuDTO;
+import dto.OrderDTO;
 import menu.MenuDetailPanel;
 
-public class MenuFrame extends JFrame {
+public class MenuFrame extends JFrame implements WindowListener{
 	
 	//윈도우 창과 레이아웃 사이의 공백사이즈가 10, 10인 카드 레이아웃 생성
 	CardLayout cl = new CardLayout(10,10);
 
 	//버튼
-	JButton btnOrder = new JButton("다음으로");
+	JButton btnNext = new JButton("다음으로");
 	JButton homeBtn = new JButton("홈으로");
 	
 	//Tabbed Panes 사용하기
@@ -57,16 +48,22 @@ public class MenuFrame extends JFrame {
 	
 	MenuDAO menuDao = new MenuDAO();
 	
-	public MenuFrame() {
+	KioskStartFrame kioskStartFrame;
+	
+	OrderDTO orderDto = new OrderDTO();
+	
+	public MenuFrame(KioskStartFrame kioskStartFrame) {
 		//기본 프레임 세팅
 		setSize(656,820);
 		setLocationRelativeTo(null);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//x버튼 눌러도 이 화면이 사라지지 않도록
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		this.addWindowListener(this);
 		setResizable(false);								//사용자가 크기 변경 불가
 		setVisible(true);
 		setTitle("메뉴 선택");
 		
-		
+		this.kioskStartFrame = kioskStartFrame;
 		
 		//각 패널에 그리드 레이아웃 적용
 		aPanel.setLayout(new GridLayout(6,3));
@@ -80,13 +77,14 @@ public class MenuFrame extends JFrame {
 		homeBtn.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				new KioskStartFrame();
+				
 				MenuFrame.this.setVisible(false);
+				kioskStartFrame.setVisible(true);
 			}
 		});
 		
 		//orderBtn버튼을 menuOrderPanel에 더하기
-		menuOrderPanel.add(btnOrder);
+		menuOrderPanel.add(btnNext);
 		
 		//각각 cPanel, fPanel, pPanel에 메뉴 정보 넣기
 		
@@ -95,10 +93,10 @@ public class MenuFrame extends JFrame {
 		setUpPanel(pPanel, "premium");
 		
 		//주문 버튼 btnOrder을 누르면 발생하는 이벤트
-		btnOrder.addActionListener(new ActionListener() {
+		btnNext.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				new OrderFrame();
+				new OrderFrame(MenuFrame.this, orderDto);
 				MenuFrame.this.setVisible(false);
 			}
 		});
@@ -123,12 +121,14 @@ public class MenuFrame extends JFrame {
 		add(tabbedPane, BorderLayout.CENTER);
 		add(menuOrderPanel, BorderLayout.SOUTH);
 		
+		
+		
 	}
 	
 	//메뉴 정보 aPanel과 해당 패널에 넣기
 	public void makePanel(JPanel panel, MenuDTO menuVo) {
-		aPanel.add(new MenuDetailPanel(menuVo));
-		panel.add(new MenuDetailPanel(menuVo));
+		aPanel.add(new MenuDetailPanel(menuVo, orderDto));
+		panel.add(new MenuDetailPanel(menuVo, orderDto));
 	}
 	
 	private void setUpPanel(JPanel panel, String pMenuCategory) {
@@ -139,6 +139,52 @@ public class MenuFrame extends JFrame {
 				this.makePanel(panel, dto);
 			}
 		}
+		
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		System.out.println("windowclosing");
+		int result = JOptionPane.showConfirmDialog(this, "종료하시겠습니까?", "프로그램 종료 확인", JOptionPane.OK_CANCEL_OPTION);
+		if ( result == JOptionPane.OK_OPTION ) {
+			System.exit(0);
+		}
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("windowClosed");
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowActivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
 }
